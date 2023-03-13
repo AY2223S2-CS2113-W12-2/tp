c89e51fcd22e9705c3679e0c12835aa7c2670727
wuzhzn <dagreatbao@gmail.com>
2023-03-13 09:42:43 +0800

    Update formatting based on gradle

--- src/main/java/eventus/Parser.java
+++ src/main/java/eventus/Parser.java
@@ -9,27 +9,32 @@ public interface Parser {
     static Command parse(String input){
         String[] inputWords = input.split(" ");
         String command = inputWords[0];
-        switch (command){
-            case "list":
-                if (inputWords[1].equals("companies")){
-                    ListCompanyCommand companyCommand = new ListCompanyCommand(command);
-                    return companyCommand;
-                }
-                else if (inputWords[1].equals("venues")) {
-                    ListVenueCommand venueCommand = new ListVenueCommand(command);
-                    return venueCommand;
-                }
-            case "add":
-                System.out.println(input.replaceFirst(command, "").trim());
-            case "delete":
-                int taskNum = Integer.parseInt(inputWords[1]) - 1;
-                DeleteCommand deleteCommand = new DeleteCommand(command, taskNum);
-                return deleteCommand;
-            case "help":
-                Ui.showGuide();
-            case "exit":
-                Ui.showExitMessage();
-            default: System.out.println("Unknown input!");
+        switch (command) {
+        case "list":
+            if (inputWords[1].equals("companies")) {
+                ListCompanyCommand companyCommand = new ListCompanyCommand(command);
+                return companyCommand;
+            } else if (inputWords[1].equals("venues")) {
+                ListVenueCommand venueCommand = new ListVenueCommand(command);
+                return venueCommand;
+            }
+            System.out.println("Unknown input!");
+            break;
+        case "add":
+            System.out.println(input.replaceFirst(command, "").trim());
+            break;
+        case "delete":
+            int taskNum = Integer.parseInt(inputWords[1]) - 1;
+            DeleteCommand deleteCommand = new DeleteCommand(command, taskNum);
+            return deleteCommand;
+        case "help":
+            Ui.showGuide();
+            break;
+        case "exit":
+            Ui.showExitMessage();
+            break;
+        default:
+            System.out.println("Unknown input!");
         }
         return null;
     }
