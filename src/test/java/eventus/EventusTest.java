14e45adba5d038c03102349c1284c9cc91cb6b55
wuzhzn <dagreatbao@gmail.com>
2023-03-13 09:47:52 +0800

    Update test folder

---
+++ src/test/java/eventus/EventusTest.java
@@ -0,0 +1,13 @@
+package eventus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventusTest {
+
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
