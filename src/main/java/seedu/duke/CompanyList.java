3882ff7a0cdaea3e76c96d653dc355b5cdb69372
Akmal_Hanis <akmalightning@gmail.com>
2023-03-14 22:29:16 +0800

    merge conflict solution

--- src/main/java/seedu/duke/CompanyList.java
+++ src/main/java/seedu/duke/CompanyList.java
@@ -6,19 +6,15 @@ import java.util.ArrayList;
 
 public class CompanyList {
 
-    private static ArrayList<Company> companyList;
+    ArrayList<Company> companies = new ArrayList<>();
 
-    public CompanyList(ArrayList<Company> companyList) {
-        this.companyList = companyList;
-    }
-
-    public static void listCompanyInformation() throws EmptyListException{
-        if (companyList.size() == 0) {
+    public void listCompanyInformation() throws EmptyListException{
+        if (companies.size() == 0) {
             throw new EmptyListException();
         }
         else {
-            for (int i = 0; i < companyList.size(); i += 1) {
-                System.out.println((i+1) + ". " + companyList.get(1));
+            for (int i = 0; i < companies.size(); i += 1) {
+                System.out.println((i+1) + ". " + companies.get(1));
             }
         }
     }
