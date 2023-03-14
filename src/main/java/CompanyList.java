8caf14d33ed2ce8a810b699e5f447ee4a915cabb
Akmal_Hanis <akmalightning@gmail.com>
2023-03-14 09:18:48 +0800

    updated print company list

--- src/main/java/CompanyList.java
+++ src/main/java/CompanyList.java
@@ -6,15 +6,12 @@ public class CompanyList {
 
     ArrayList<Company> companies = new ArrayList<>();
 
-    public void list(){
-        if (companies.size() == 0){
-            System.out.println("You do not have any companies in this list");
-        }
+    public void listCompanInyformation(){
+        if (companies.size() == 0) {
+            System.out.println("You do not have any companies in this list");}
         else {
             for (int i = 0; i < companies.size(); i += 1) {
-                System.out.println((i+1) + ". " + companies.get(1));
-
-            }
+                System.out.println((i+1) + ". " + companies.get(1));}
         }
     }
 }
